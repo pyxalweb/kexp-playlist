05ada89573c2bc66cddb322c175b051be9ad2942
Alex Winter <alex@pyxalweb.com>
2023-05-30 23:39:17 -0700

    Added print commands to debug

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
+print('1. successfully ran: from selenium import webdriver')
 
 driver = webdriver.Chrome()
+print('2. successfully ran: driver = webdriver.Chrome()')
 
 driver.get('https://www.kexp.org/playlist/')
-
-print('test')
+print('3. successfully ran: driver.get(https://www.kexp.org/playlist/)')
