1fd11275e83a4125d52ed0430c629138936ff4b8
Alex Winter <alex@pyxalweb.com>
2023-05-30 23:57:02 -0700

    Added print success messages and max_loops

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
 from selenium import webdriver
-from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+print('Dependencies imported successfully.')
 
 options = Options()
 options.add_argument("--headless")
 driver = webdriver.Chrome(options=options)
+print('Chrome WebDriver is ready.')
 
 driver.get('https://www.kexp.org/playlist/')
 page_source = driver.page_source
+print('Selenium has the page source.')
+
 soup = BeautifulSoup(page_source, 'html.parser')
+print('BeautifulSoup has parsed the HTML.')
+
+print('The script has finished successfully!')
+
+max_loops = 1
+loop_count = 0
+print(f'We will scrape {max_loops} playlist page(s).')
