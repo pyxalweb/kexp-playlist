d54b94ddb620b5d4992581357a79ed3b152c3fb2
Alex Winter <alex@pyxalweb.com>
2023-05-30 23:48:01 -0700

    Add beautifulsoup

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
 print('1. successfully ran')
 
 from selenium.webdriver.chrome.options import Options
@@ -18,3 +19,6 @@ print('6. successfully ran')
 
 page_source = driver.page_source
 print('Page source is now available')
+
+soup = BeautifulSoup(page_source, 'html.parser')
+print('Parsed page source with BeautifulSoup')
