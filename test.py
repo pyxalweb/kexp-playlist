f6ae9c548e8976f792c3b7b52fb0e296940c769a
Alex Winter <alex@pyxalweb.com>
2023-05-30 23:42:27 -0700

    import options

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 from selenium import webdriver
-print('1. successfully ran: from selenium import webdriver')
+print('1. successfully ran')
 
-driver = webdriver.Chrome()
-print('2. successfully ran: driver = webdriver.Chrome()')
+from selenium.webdriver.chrome.options import Options
+print('2. successfully ran')
+
+options = Options()
+print('3. successfully ran')
+
+options.add_argument("--headless")
+print('4. successfully ran')
+
+driver = webdriver.Chrome(options=options)
+print('5. successfully ran')
 
 driver.get('https://www.kexp.org/playlist/')
-print('3. successfully ran: driver.get(https://www.kexp.org/playlist/)')
+print('6. successfully ran')
