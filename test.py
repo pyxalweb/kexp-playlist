12f19dddb1f9aa73818009f632d6e06567627233
Alex Winter <alex@pyxalweb.com>
2023-05-31 00:42:45 -0700

    Added comments, added previous_link click, set to 3 max_loops

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ print('Chrome WebDriver is ready.')
 driver.get('https://www.kexp.org/playlist/')
 print('Selenium has the URL ready.')
 
-max_loops = 1
+max_loops = 3
 loop_count = 0
 print(f'We will scrape {max_loops} playlist page(s).')
 
@@ -41,14 +41,23 @@ while loop_count < max_loops:
             primary_content_div = playlist_item.find('div', class_='PlaylistItem-primaryContent')
             print('Found content within a playlist item.')
 
+            # If the 'PlaylistItem-primaryContent' div does not contain the appropriate text (track name, artist name, year) then it is likely a sponsor, air break, etc and should be skipped
             if primary_content_div.find('h3', class_='u-mb0' != None) and (primary_content_div.find('div', class_='u-mb1') != None) and (primary_content_div.find_all('div', class_='u-h5') != []):
+                # Get the track name, artist name, and year
                 track = primary_content_div.find('h3', class_='u-mb0').get_text(strip=True)
                 artist = primary_content_div.find('div', class_='u-mb1').get_text(strip=True)
                 year = primary_content_div.find_all('div', class_='u-h5')[-1].get_text(strip=True)
+                # If the year is matched, then proceed
                 if re.match('^2023', year):
+                    # Add it to the array
                     items.append((track, artist))
-                    print(f'Appended {track} by {artist} to array')
+                    print(f'Appended {track} by {artist} to array.')
 
+        # Click the 'previous' link to go to the next playlist page
+        previous_link = driver.find_element(By.ID, "previous")
+        previous_link.click()
+
+        # Add 1 to the loop_count
         loop_count += 1
     except Exception as e:
         print('Error:', e)
